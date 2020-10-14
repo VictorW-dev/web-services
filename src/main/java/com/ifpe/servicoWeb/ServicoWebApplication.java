ad318914f62d04fbd69d48fe311a2fcb45af4030
Victor Winicius <victorwinicius.bs@gmail.com>
2020-10-14 20:33:08 -0300

    Project Created

---
+++ src/main/java/com/ifpe/servicoWeb/ServicoWebApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.servicoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoWebApplication.class, args);
+	}
+
+}
