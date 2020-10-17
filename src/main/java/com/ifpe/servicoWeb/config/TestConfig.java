e60aaf55e543987bb209d4ad7b1db564840c0119
Victor Winicius <victorwinicius.bs@gmail.com>
2020-10-17 17:32:58 -0300

    OrderStatus Enum

--- src/main/java/com/ifpe/servicoWeb/config/TestConfig.java
+++ src/main/java/com/ifpe/servicoWeb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.ifpe.servicoWeb.entities.Order;
 import com.ifpe.servicoWeb.entities.User;
+import com.ifpe.servicoWeb.entities.enums.OrderStatus;
 import com.ifpe.servicoWeb.repositories.OrderRepository;
 import com.ifpe.servicoWeb.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Clara", "mariac@gmail.com", "400289226", "123476");
 		User u2 = new User(null, "Alex Morgan", "alex@gmail.com", "123456789", "127456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2020-09-17T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-10-16T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2020-10-15T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2020-09-17T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-10-16T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2020-10-15T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
