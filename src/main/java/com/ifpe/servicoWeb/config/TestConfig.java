c79c7e4b4a38fc55e6b042a9b9cdb02b14cced63
Victor Winicius <victorwinicius.bs@gmail.com>
2020-10-17 23:29:03 -0300

    Product

--- src/main/java/com/ifpe/servicoWeb/config/TestConfig.java
+++ src/main/java/com/ifpe/servicoWeb/config/TestConfig.java
@@ -10,46 +10,56 @@ import org.springframework.context.annotation.Profile;
 
 import com.ifpe.servicoWeb.entities.Category;
 import com.ifpe.servicoWeb.entities.Order;
+import com.ifpe.servicoWeb.entities.Product;
 import com.ifpe.servicoWeb.entities.User;
 import com.ifpe.servicoWeb.entities.enums.OrderStatus;
 import com.ifpe.servicoWeb.repositories.CategoryRepository;
 import com.ifpe.servicoWeb.repositories.OrderRepository;
+import com.ifpe.servicoWeb.repositories.ProductRepository;
 import com.ifpe.servicoWeb.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers"); 
-		
+		Category cat3 = new Category(null, "Computers");
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(null, "Maria Clara", "mariac@gmail.com", "400289226", "123476");
-		User u2 = new User(null, "Alex Morgan", "alex@gmail.com", "123456789", "127456"); 
-		
+		User u2 = new User(null, "Alex Morgan", "alex@gmail.com", "123456789", "127456");
+
 		Order o1 = new Order(null, Instant.parse("2020-09-17T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2020-10-16T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2020-10-15T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
-		
+		Order o3 = new Order(null, Instant.parse("2020-10-15T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
+
 	}
-	
-	
 
 }
